1ec2ad96cec784182bd7340a825fcb74533f3d02
Salvador Montiel <salvadormontiel8@gmail.com>
2017-11-08 15:14:27 -0600

    First commit

---
+++ app/src/main/java/com/smontiel/kanbanboard/Task.java
@@ -0,0 +1,16 @@
+package com.smontiel.kanbanboard;
+
+/**
+ * Created by Salvador Montiel on 8/11/17.
+ */
+public class Task {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
