288a39cc160f1ff2acdf0ec96f957ad4135e0665
Salvador Montiel <salvadormontiel8@gmail.com>
2017-11-14 14:32:06 -0600

    Update to a MVP architecture

---
+++ app/src/main/java/com/smontiel/kanbanboard/BasePresenter.java
@@ -0,0 +1,12 @@
+package com.smontiel.kanbanboard;
+
+/**
+ * Created by Salvador Montiel on 14/11/17.
+ */
+public interface BasePresenter {
+
+    void subscribe();
+
+    void unsubscribe();
+
+}
