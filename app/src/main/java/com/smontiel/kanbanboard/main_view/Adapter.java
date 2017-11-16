bef216c15918402b400254480efec34a64861632
Salvador Montiel <salvadormontiel8@gmail.com>
2017-11-16 14:29:35 -0600

    Refactorized

--- app/src/main/java/com/smontiel/kanbanboard/main_view/Adapter.java
+++ app/src/main/java/com/smontiel/kanbanboard/main_view/Adapter.java
@@ -3,6 +3,7 @@ package com.smontiel.kanbanboard.main_view;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.util.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,30 +12,35 @@ import java.util.List;
  * Created by Salvador Montiel on 11/11/17.
  */
 class Adapter extends FragmentPagerAdapter {
-    private final List<Fragment> mFragments = new ArrayList<>();
-    private final List<String> mFragmentTitles = new ArrayList<>();
+    private final List<Pair<TasksFragment, Long>> fragments = new ArrayList<>();
+    private final List<String> fragmentTitles = new ArrayList<>();
 
     public Adapter(FragmentManager fm) {
         super(fm);
     }
 
-    public void addFragment(Fragment fragment, String title) {
-        mFragments.add(fragment);
-        mFragmentTitles.add(title);
+    public void addFragment(TasksFragment fragment, String title, long idColumn) {
+        Pair<TasksFragment, Long> pair = new Pair<>(fragment, idColumn);
+        fragments.add(pair);
+        fragmentTitles.add(title);
+    }
+
+    public long getFragmentIdColumn(int position) {
+        return fragments.get(position).second;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return mFragments.get(position);
+        return fragments.get(position).first;
     }
 
     @Override
     public int getCount() {
-        return mFragments.size();
+        return fragments.size();
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return mFragmentTitles.get(position);
+        return fragmentTitles.get(position);
     }
 }
